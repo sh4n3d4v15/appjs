b14f40bf5ace6aa1cd7786e40c6a26dc6a19a3bc
unknown <daviess@C61PWW1.chep.com>
2013-03-23 18:01:29 +0000

    parser modified

---
+++ data/app.js
@@ -0,0 +1,120 @@
+var app = module.exports = require('appjs');
+var fs = require('fs');
+var moongoose = require('mongoose');
+
+var log = fs.createWriteStream('log.txt',{'flags':'w'})
+
+
+
+
+app.serveFilesFrom(__dirname + '/content');
+
+var menubar = app.createMenu([{
+  label:'&File',
+  submenu:[
+    {
+      label:'E&xit',
+      action: function(){
+        window.close();
+      }
+    }
+  ]
+},{
+  label:'&Window',
+  submenu:[
+    {
+      label:'Fullscreen',
+      action:function(item) {
+        window.frame.fullscreen();
+        console.log(item.label+" called.");
+      }
+    },
+    {
+      label:'Minimize',
+      action:function(){
+        window.frame.minimize();
+      }
+    },
+    {
+      label:'Maximize',
+      action:function(){
+        window.frame.maximize();
+      }
+    },{
+      label:''//separator
+    },{
+      label:'Restore',
+      action:function(){
+        window.frame.restore();
+      }
+    }
+  ]
+}]);
+
+menubar.on('select',function(item){
+  console.log("menu item "+item.label+" clicked");
+});
+
+var trayMenu = app.createMenu([{
+  label:'Show',
+  action:function(){
+    window.frame.show();
+  },
+},{
+  label:'Minimize',
+  action:function(){
+    window.frame.hide();
+  }
+},{
+  label:'Exit',
+  action:function(){
+    window.close();
+  }
+}]);
+
+var statusIcon = app.createStatusIcon({
+  icon:'./data/content/icons/32.png',
+  tooltip:'AppJS Hello World',
+  menu:trayMenu
+});
+
+var window = app.createWindow({
+  width  : 640,
+  height : 460,
+  disableSecurity:true,
+  icons  : __dirname + '/content/icons'
+});
+
+window.on('create', function(){
+  console.log("Window Created");
+  window.frame.show();
+  window.frame.center();
+  window.frame.setMenuBar(menubar);
+});
+
+window.on('ready', function(){
+  console.log("Window Ready");
+  window.process = process;
+  window.module = module;
+
+  function F12(e){ return e.keyIdentifier === 'F12' }
+  function Command_Option_J(e){ return e.keyCode === 74 && e.metaKey && e.altKey }
+
+  window.addEventListener('keydown', function(e){
+    if (F12(e) || Command_Option_J(e)) {
+      window.frame.openDevTools();
+    }
+  });
+});
+
+window.on('close', function(){
+  console.log("Window Closed");
+});
+
+
+
+app.router.post('/',function(req,res,next){
+  log.write(req.body.firstname)
+  res.send(req.body.firstname);
+  console.log('shane')
+})
